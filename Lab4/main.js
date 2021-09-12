5dd26ca5b9ae65f5d7cab0d0a6da390a93cc8bba
Honguito <eduardolalojami@gmail.com>
2021-09-11 19:52:42 -0500

    factory pattern and RORO

---
+++ Lab4/main.js
@@ -0,0 +1,93 @@
+function isObject(subject) {
+    return typeof subject == "object";
+}
+function isArray(subject) {
+    return Array.isArray(subject);
+}
+function deepCopy(subject) {
+    let copySubject;
+    const subjectIsArray = isArray(subject);
+    const subjectIsObject = isObject(subject);
+    if (subjectIsArray) {
+        copySubject = []
+    } else if (subjectIsObject) {
+        copySubject = {}
+    } else {
+        return subject;
+    }
+    for (key in subject){
+        const keyIsObject = isObject(subject[key]);
+        if (keyIsObject){
+            copySubject[key] = deepCopy(subject[key]);
+        } else {
+            if(subjectIsArray){
+                copySubject.push(subject[key]);
+            } else {
+                copySubject[key] = subject[key];
+            }
+        }
+    }
+    return copySubject;
+}
+
+// const StudentBase = {
+//     name: undefined,
+//     email: undefined,
+//     age: undefined,
+//     approvedCourses: undefined,
+//     learningPaths: undefined,
+//     socialMedia: {
+//         twitter: undefined,
+//         instagram: undefined,
+//         facebook: undefined,
+//     }
+// };
+
+
+// const juan = deepCopy(StudentBase);
+
+// Object.defineProperty(juan, "name", {
+//     value: "Juanito",
+//     configurable: false,
+// });
+// Object.seal(juan);
+function requiredParam(param) {
+    throw new Error(param + " es obligatorio");
+}
+
+function createStudent(
+    {
+        name = requiredParam("name"),
+        email = requiredParam("email"),
+        age,
+        twitter,
+        instagram,
+        facebook,
+        approvedCourses = [],
+        learningPaths = [],
+    } = {}
+) {
+    return {
+        name,
+        email,
+        age,
+        approvedCourses,
+        learningPaths,
+        socialMedia: {
+            twitter,
+            instagram,
+            facebook,
+        },
+    };
+}
+// const juan = createStudent({
+//     name: "Juanito",
+//     age: 18,
+//     email: "JDC@gmail.com",
+//     twitter: "fjuandc",
+// });
+
+const juan  = createStudent({
+    name: "Juanito",
+    email: "jdc@gmail.com"
+});
